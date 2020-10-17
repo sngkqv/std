e3cf1644c177b2fe32a83452a1b09e533bc5056d
muskan0048 <muskan1998>
2020-10-17 21:51:42 +0530

    Updates

---
+++ precache-manifest.dedbe232ad9c62e00af795b02ec00be7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45f60f6601bbcfc4d87f555eefa10d06",
+    "url": "/std/index.html"
+  },
+  {
+    "revision": "94014a2421c16e23ea93",
+    "url": "/std/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ab27cf072c5a9ee0d7dc",
+    "url": "/std/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "94014a2421c16e23ea93",
+    "url": "/std/static/js/2.454ef619.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/std/static/js/2.454ef619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab27cf072c5a9ee0d7dc",
+    "url": "/std/static/js/main.516738a7.chunk.js"
+  },
+  {
+    "revision": "253fb953ac78813665c5",
+    "url": "/std/static/js/runtime-main.7f762750.js"
+  }
+]);
